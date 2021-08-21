213b401d24131f3d12dccc1659f9ef8c696b916c
Benjamin <benjaminx@gmail.com>
2021-08-21 10:01:08 +0800

    distfeeds.conf

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,6 +14,9 @@
 sed -i 's/192.168.1.1/10.1.1.1/g' package/base-files/files/bin/config_generate
 echo '### Updates default IP gate ###'
 
+sed -i 's#mirrors.cloud.tencent.com/lede#downloads.openwrt.org#g' /etc/opkg/distfeeds.conf
+echo '### Updates distfeeds.conf ###'
+
 rm -rf package/lean/luci-theme-argon && rm -rf package/lean/luci-theme-material && rm -rf package/lean/luci-theme-netgear
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 echo '### Updates Theme Argon ###'
