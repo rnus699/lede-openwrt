7bfa1b2555497b940c2e65970b7d2c6b89cf703f
Benjamin <benjaminx@gmail.com>
2021-08-25 21:53:13 +0800

    Testing

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,8 +18,8 @@ rm -rf package/lean/luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 echo '### Updates Theme Argon ###'
 
-git clone https://github.com/jerrykuku/luci-app-argon-config.git package/lean/luci-app-argon-config
-echo '### Argon Theme Config ###'
+# git clone https://github.com/jerrykuku/luci-app-argon-config.git package/lean/luci-app-argon-config
+# echo '### Argon Theme Config ###'
 
-git clone https://github.com/esirplayground/luci-app-poweroff.git package/lean/luci-app-poweroff
-echo '### Shutdown Router ###'
+# git clone https://github.com/esirplayground/luci-app-poweroff.git package/lean/luci-app-poweroff
+# echo '### Shutdown Router ###'
