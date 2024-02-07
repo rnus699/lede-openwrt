dd6f3882078c2c0b8ef1856b98b57b4259ad85dd
rnus699 <150065716+rnus699@users.noreply.github.com>
2024-02-07 21:46:09 +0800

    Add files via upload

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,10 +14,11 @@
 # echo '### Updates default IP gate ###'
 # sed -i 's/192.168.1.1/10.1.1.10/g' package/base-files/files/bin/config_generate
 # echo '###  ###'
-
+sed -i 's/stripped/release/g' feeds/packages/multimedia/aliyundrive-webdav/Makefile
 # echo '### Updates Theme Argon ###'
 # package/lean/luci-theme-argon /package/feeds/luci/luci-theme-argon
 # rm -rf package/feeds/luci/luci-theme-argon
-git clone -b luci-smartdns-dev https://github.com/xiaorouji/openwrt-passwall.git package/passwall
+# git clone https://github.com/xiaozhuai/luci-app-filebrowser package/luci-app-filebrowser
+# git clone -b luci-smartdns-dev https://github.com/xiaorouji/openwrt-passwall.git package/passwall
 # git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
 # echo '###  ###'
