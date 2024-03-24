ddf762b5f3756ecff733621fefca3c486b56bc4d
rnus699 <150065716+rnus699@users.noreply.github.com>
2024-03-24 16:39:21 +0800

    Add files via upload

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,11 +14,17 @@
 # echo '### Updates default IP gate ###'
 # sed -i 's/192.168.1.1/10.1.1.10/g' package/base-files/files/bin/config_generate
 # echo '###  ###'
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 sed -i 's/stripped/release/g' feeds/packages/multimedia/aliyundrive-webdav/Makefile
+sed -i 's#GO_PKG_TARGET_VARS.*# #g' feeds/packages/utils/v2dat/Makefile
+# sed -i 's/CGO_ENABLED=0/CGO_ENABLED=1/g' feeds/packages/net/mosdns/Makefile
 # echo '### Updates Theme Argon ###'
 # package/lean/luci-theme-argon /package/feeds/luci/luci-theme-argon
 # rm -rf package/feeds/luci/luci-theme-argon
 # git clone https://github.com/xiaozhuai/luci-app-filebrowser package/luci-app-filebrowser
-git clone -b luci-smartdns-dev https://github.com/xiaorouji/openwrt-passwall.git package/passwall
+# git clone -b luci-smartdns-dev https://github.com/xiaorouji/openwrt-passwall package/passwall
+# git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+# git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 # git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
 # echo '###  ###'
